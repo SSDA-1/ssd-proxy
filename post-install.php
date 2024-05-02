b290eee60e9293477389a114f4bc97a280b9b2e4
Don410 <don_410@mail.ru>
2024-05-02 20:57:57 +0300

    Update post-install.php

--- post-install.php
+++ post-install.php
@@ -2,12 +2,9 @@
 $configPath = __DIR__ . '/../../../../bootstrap/providers.php';
 $provider = "ssda1\\proxies\\Providers\\ProxyServiceProvider::class";
 
-// Загрузка текущего содержимого файла конфигурации
 $config = file_get_contents($configPath);
 
-// Проверка, содержит ли файл уже указанный провайдер
 if (!str_contains($config, $provider)) {
-    // Ищем закрывающую скобку массива и добавляем перед ней новый провайдер
     $config = str_replace("];", "    $provider,\n];", $config);
     file_put_contents($configPath, $config);
 }
