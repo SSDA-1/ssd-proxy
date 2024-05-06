3bbee08885266c9e9e86d2d7551b543fcc7d1844
Tlaiw <76945536+tlaiw-code@users.noreply.github.com>
2024-05-06 13:11:51 +0300

    Update TemplateController.php
    use Illuminate\Support\Facades\Schema;

--- src/Http/Controllers/TemplateController.php
+++ src/Http/Controllers/TemplateController.php
@@ -14,6 +14,7 @@ use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class TemplateController extends Controller
 {
