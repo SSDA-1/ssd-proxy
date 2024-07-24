75be3ecab0db2ac9ecb84b4e056ef395c65a81b2
Don410 <don_410@mail.ru>
2024-07-24 22:46:29 +0300

    Update PermissionTableSeeder.php

--- src/Database/Seeders/PermissionTableSeeder.php
+++ src/Database/Seeders/PermissionTableSeeder.php
@@ -4,7 +4,6 @@ namespace Ssda1\proxies\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
-use Illuminate\Support\Facades\Log;
 
 class PermissionTableSeeder extends Seeder
 {
@@ -61,9 +60,6 @@ class PermissionTableSeeder extends Seeder
             $perm = Permission::where('name', $permission)->first();
 
             if ($perm) {
-                // Log current guard_name
-                Log::info("Permission '{$perm->name}' has guard '{$perm->guard_name}'");
-
                 $perm->guard_name = 'web';
                 $perm->save();
             } else {
