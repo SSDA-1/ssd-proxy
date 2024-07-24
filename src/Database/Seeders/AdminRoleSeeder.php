9e7b20027a92c1e8d8e2fea3b51368d31158f8f3
Don410 <don_410@mail.ru>
2024-07-24 22:40:08 +0300

    Update AdminRoleSeeder.php

--- src/Database/Seeders/AdminRoleSeeder.php
+++ src/Database/Seeders/AdminRoleSeeder.php
@@ -17,6 +17,22 @@ class AdminRoleSeeder extends Seeder
      */
     public function run()
     {
+        // Update existing permissions to use guard 'web'
+        Permission::all()->each(function ($permission) {
+            if ($permission->guard_name != 'web') {
+                $permission->guard_name = 'web';
+                $permission->save();
+            }
+        });
+
+        // Update existing roles to use guard 'web'
+        Role::all()->each(function ($role) {
+            if ($role->guard_name != 'web') {
+                $role->guard_name = 'web';
+                $role->save();
+            }
+        });
+
         // Check if user already exists
         $user = User::where('email', 'admin@admin.com')->first();
 
