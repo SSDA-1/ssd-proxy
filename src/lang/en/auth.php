c681dc6dc1bdcc86d6220d15e7a44fc98114b4a8
Don410 <don_410@mail.ru>
2025-04-11 16:31:33 +0300

    Update auth.php

--- src/lang/en/auth.php
+++ src/lang/en/auth.php
@@ -13,8 +13,24 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
+    'failed' => 'Incorrect login or password.',
+    'password' => 'The password provided is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
+    'Reset Password' => 'Password reset',
+    'Email Address' => 'Email Address',
+    'Reset the password' => 'Reset password',
+    'Password' => 'Password',
+    'Confirm Password' => 'Confirm your password',
+    'Please confirm your password before continuing.' => 'Please confirm your password before continuing.',
+    'Forgot Your Password?' => 'Forgot your password?',
+    'Remember Me' => 'Remember me',
+    'Login' => 'Login',
+    'Register' => 'Registration',
+    'Registers' => 'Register',
+    'Name' => 'UserName',
+    'Verify Your Email Address' => 'Check your email address',
+    'A fresh verification link has been sent to your email address.' => 'A new confirmation link has been sent to your email address.',
+    'Before proceeding, please check your email for a verification link.' => 'Before proceeding, please check your email for a confirmation link.',
+    'If you did not receive the email' => 'If you haven`t received the letter',
+    'click here to request another' => 'Click here to request another',
 ];
