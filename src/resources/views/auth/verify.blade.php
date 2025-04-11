8209a1b59db32ec8b35fbfc320cfcdbe569011b8
Don410 <don_410@mail.ru>
2025-04-11 17:33:52 +0300

    Update verify.blade.php

--- src/resources/views/auth/verify.blade.php
+++ src/resources/views/auth/verify.blade.php
@@ -6,22 +6,22 @@
 
 <div class="wraper-lk-block">
     <div class="card-header">
-        <h1>{{ __('auth.Verify Your Email Address') }}</h1>
+        <h1>Verify Your Email Address</h1>
     </div>
 
     <div class="wraper-lk-content">
         @if (session('resent'))
         <div class="alert alert-success" role="alert">
-            {{ __('auth.A fresh verification link has been sent to your email address.') }}
+            A fresh verification link has been sent to your email address.
         </div>
         @endif
 
-        {{ __('auth.Before proceeding, please check your email for a verification link.') }}
-        {{ __('auth.If you did not receive the email') }},
+        Before proceeding, please check your email for a verification link.
+        If you did not receive the email,
         <form class="d-inline" method="POST" action="{{ route('verification.resend') }}" data-fetch="none">
             @csrf
             <div class="left-block-lk">
-                <button type="submit" class="btn button">{{ __('auth.click here to request another') }}</button>.
+                <button type="submit" class="btn button">click here to request another</button>.
             </div>
         </form>
     </div>
