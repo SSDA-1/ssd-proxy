b2d214087646db084313961a9990d57fb4921485
Don410 <don_410@mail.ru>
2025-04-11 17:34:44 +0300

    Update confirm.blade.php

--- src/resources/views/auth/passwords/confirm.blade.php
+++ src/resources/views/auth/passwords/confirm.blade.php
@@ -5,17 +5,17 @@
 @section('content')
     <div class="wraper-lk-block">
         <div class="card-header">
-            <h1>{{ __('auth.Confirm Password') }}</h1>
+            <h1>Confirm Password</h1>
         </div>
 
         <div class="wraper-lk-content">
-            {{ __('auth.Please confirm your password before continuing.') }}
+            Please confirm your password before continuing.
 
             <form method="POST" action="{{ route('password.confirm') }}" data-fetch="none">
                 @csrf
 
                 <div class="left-block-lk">
-                    <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('auth.Password') }}</label>
+                    <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
 
                     <div class="col-md-6">
                         <input id="password" type="password" class="input-lk @error('password') is-invalid @enderror"
@@ -30,14 +30,14 @@
                 </div>
 
                 <button type="submit" class="btn button">
-                    {{ __('auth.Confirm Password') }}
+                    Confirm Password
                 </button>
 
                 <div class="wrap-reg-form">
                     <div class="reg login-reg">
                         @if (Route::has('password.request'))
                             <a class="btn btn-link" href="{{ route('password.request') }}">
-                                {{ __('auth.Forgot Your Password?') }}
+                                Forgot Your Password?
                             </a>
                         @endif
                     </div>
